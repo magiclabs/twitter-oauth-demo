c374219e0b377036549eb1c12c9c986cc7c2ec4d
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-05-05 09:44:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Magic Auth Twitter Social Login
+
+Please [click here](https://magic.link/docs/auth/login-methods/social-logins/integration/social-providers/twitter) for full implementation instructions
---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import { magic } from "../lib/magic";
+import { FaTwitter } from "react-icons/fa";
+
+const Login = () => {
+  const handleSocialLogin = async () => {
+    try {
+      await magic.oauth.loginWithRedirect({
+        provider: "twitter",
+        redirectURI: new URL("/dashboard", window.location.origin).href,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Magic Auth + Twitter</h1>
+      <button onClick={handleSocialLogin}>
+        <FaTwitter size={"2.5rem"} />
+        Log in with Twitter
+      </button>
+    </div>
+  );
+};
+
+export default Login;
